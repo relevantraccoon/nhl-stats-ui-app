c8b1caefc72588ba06d44ceaf8b52e4b38ae361c
Joakim Karlsson <joakim.t.karlsson.1985@gmail.com>
2022-11-15 19:45:26 +0100

    Minor change to Leaderslist.jsx

--- src/Components/Leaderslist.jsx
+++ src/Components/Leaderslist.jsx
@@ -1,20 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Leaderslist = () => {
   return (
     <div id="leaders-playerlist-div">
-      <ul id="leaders-list">
-        <li id="leaders-list-playername">Nino Niederreiter</li>
-        <li id="leaders-list-playername">Kiefer Sherwood</li>
-        <li id="leaders-list-playername">Eeli Tolvanen</li>
-        <li id="leaders-list-playername">Mario Ferraro</li>
-        <li id="leaders-list-playername">Luke Kunin</li>
-        <li id="leaders-list-playername">Filip Forsberg</li>
-        <li id="leaders-list-playername">Mikael Granlund</li>
-        <li id="leaders-list-playername">Matt Duchene</li>
-        <li id="leaders-list-playername">Ryan McDonagh</li>
-        <li id="leaders-list-playername">Cole Smith</li>
-      </ul>
+      <ul id="leaders-list"></ul>
       <p>All leaders</p>
     </div>
   );
