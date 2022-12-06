1b0f3e28d9809848df37b3fcc0ad67e905213399
Joakim Karlsson <joakim.t.karlsson.1985@gmail.com>
2022-12-06 17:04:51 +0100

    Add React Router

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,20 +1,29 @@
 import React from "react";
-import Positioncard from "./Positioncard";
-import { useGlobalContext } from "../context";
+// import { useGlobalContext } from "../context";
+import { Routes, Route } from "react-router-dom";
+import Leaderstart from "./Leaderstart";
+import Leaderselections from "./Leaderselections";
 
 const Main = () => {
-  const { loading, leaders } = useGlobalContext();
+  // const { loading, leaders } = useGlobalContext();
 
-  const allPlayerTypes = leaders.map((playerType) => {
-    return playerType.playerType;
-  });
+  // const allPlayerTypes = leaders.map((playerType) => {
+  //   return playerType.playerType;
+  // });
 
   return (
     <main id="main">
       {/* {allPlayerTypes.map((playerType, index) => {
         return <Positioncard key={index} playerType={playerType} />;
       })} */}
-      <Positioncard playerType={"skaters"} />
+      {/* <Positioncard playerType={"skaters"} /> */}
+      <Routes>
+        <Route path="/:season/:gameType/" element={<Leaderstart />} />
+        <Route
+          path="/:season/:gameType/:franchise/"
+          element={<Leaderselections />}
+        />
+      </Routes>
     </main>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import "modern-normalize";
 import "./styles/css/small.css";
 import App from "./App";
-import { AppProvider } from "./context";
+import { AppContext, AppProvider } from "./context";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
